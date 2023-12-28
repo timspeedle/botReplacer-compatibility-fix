7128155ed510026a11db4bd70063fee63a13275f
timspeedle <102270588+timspeedle@users.noreply.github.com>
2023-12-27 20:04:50 -0500

    Version 1.0.0
    Initial commit of version 1.0.0

---
+++ data.lua
@@ -0,0 +1,7 @@
+if data.raw["logistic-container"] and data.raw["logistic-container"]["logistic-chest-botUpgrader"] then
+    data.raw["logistic-container"]["logistic-chest-botUpgrader"].icon_size = 64
+end
+
+if data.raw["item"] and data.raw["item"]["logistic-chest-botUpgrader"] then
+    data.raw["item"]["logistic-chest-botUpgrader"].icon_size = 64
+end
